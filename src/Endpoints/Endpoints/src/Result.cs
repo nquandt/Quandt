3520482c564a276d0f09181dee58f80e99c4accc
Nicholas Quandt <nicholas.quandt@milwaukeetool.com>
2023-01-19 11:54:25 -0600

    add jsonorder 1.0.5

--- src/Endpoints/Endpoints/src/Result.cs
+++ src/Endpoints/Endpoints/src/Result.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Quandt.Endpoints
@@ -16,13 +17,17 @@ namespace Quandt.Endpoints
 
     public class Result
     {
+        [JsonPropertyOrder(order: 0)]
         public Status Status { get; set; } = Status.SUCCESS;
-        public object? Error { get; set; } = null;
+        [JsonPropertyOrder(order: 1)]
         public string? Message { get; set; } = null;
+        [JsonPropertyOrder(order: 2)]
+        public object? Error { get; set; } = null;                
     }
 
     public class Result<T> : Result where T : class
     {
+        [JsonPropertyOrder(order: 3)]
         public T? Data { get; set; } = null;
     }
 }
